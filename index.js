d3ccd9850093a9a5389308f4ebda5c6b9d4a44b2
Kaisar-byte <mohammadkaisarhamid@gmail.com>
2022-12-14 02:11:17 +0600

    vercel json file included

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
+const courses = require("./data/courses.json");
 
-app.get('/', (req, res) => {
-    res.send('Assignment -10 server is running')
-})
+app.get("/", (req, res) => {
+	res.send("programming zone server is running");
+});
 
-app.get(port, (req, res) => {
-    console.log(`Assignment started and running on port ${port}`);
-})
+app.get("/courses", (req, res) => {
+	res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+	const id = parseInt(req.params.id);
+	const course = courses.find((course) => course.id === id);
+	res.send(course);
+});
+
+app.listen(port, (req, res) => {
+	console.log(`programming zone website started and running on port ${port}`);
+});
